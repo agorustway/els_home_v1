d5448b2ba4d3a8044c41546c5ef00f49a4d0a28a
agorustway <agorustway@gmail.com>
2026-01-23 18:46:36 +0900

    초도진행v57-배포오류수정

--- web/app/api/board/route.js
+++ web/app/api/board/route.js
@@ -1,9 +1,6 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 
-import { NextResponse } from 'next/server';
-import { createClient } from '@/utils/supabase/server';
-
 export async function GET(request) {
     const supabase = await createClient();
     const { searchParams } = new URL(request.url);
