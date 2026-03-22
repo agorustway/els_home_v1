dbe4a65649f1eadc7b263509f18ba69eee00b3c9
agorustway <agorustway@gmail.com>
2026-03-22 13:50:32 +0900

    fix: FloatingWidgetService 자바 컴파일 에러 수정

--- web/android/app/src/main/java/com/elssolution/driver/FloatingWidgetService.java
+++ web/android/app/src/main/java/com/elssolution/driver/FloatingWidgetService.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ServiceInfo;
 import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
