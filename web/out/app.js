4e7632942110b646cb5bc7dc018ceea7389dda4e
agorustway <agorustway@gmail.com>
2026-03-29 04:16:37 +0900

    [v4.2.4] 날짜월별 검색칸 placeholder 미표시 오류 핫픽스 (Build 148)

--- web/out/app.js
+++ web/out/app.js
@@ -4,10 +4,10 @@
  */
 (function () {
   'use strict';
-  console.log('ELS Driver App Loading... v4.2.3');
+  console.log('ELS Driver App Loading... v4.2.4');
  
-  const APP_VERSION = 'v4.2.3';
-  const BUILD_CODE = 147; // Build 147 (v4.2.3)
+  const APP_VERSION = 'v4.2.4';
+  const BUILD_CODE = 148; // Build 148 (v4.2.4)
   const BASE_URL = 'https://www.nollae.com';
   const VERSION_URL = BASE_URL + '/apk/version.json';
 
